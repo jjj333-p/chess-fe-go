db952df2e5b9c15cd90d53168b27d56c70a7c966
jjj333_p <jjj333.p.1325@gmail.com>
2025-04-20 23:35:18 -1000

    Add chessboard, tiles, and piece initialization logic
    Introduced `ChessBoard`, `ChessTile`, and updated `ChessPiece` to support chessboard setup and UI elements. Added grid-based layout and button interactions utilizing the Fyne framework for rendering. The initialization ensures proper configuration of pieces and their corresponding tiles.

--- chessboard/main.go
+++ chessboard/main.go
@@ -1,6 +1,13 @@
 package chessboard
 
-import "fyne.io/fyne/v2/canvas"
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
 
 type ChessPiece struct {
 	black   bool
@@ -8,8 +15,13 @@ type ChessPiece struct {
 	imageEL *canvas.Image
 }
 
-func newDefaultPieceForPosition(x int, y int) *ChessPiece {
+func NewDefaultPieceForPosition(x int, y int) *ChessPiece {
 	newPiece := ChessPiece{}
+
+	if y > 1 && y < 6 {
+		return &newPiece
+	}
+
 	//top side is black
 	newPiece.black = y > 3
 
@@ -43,4 +55,64 @@ func newDefaultPieceForPosition(x int, y int) *ChessPiece {
 
 	//new instance of newPiece
 	return &newPiece
+}
+
+type ChessTile struct {
+	piece  *ChessPiece
+	button *widget.Button
+	uiEL   *fyne.Container
+}
+
+func initChessTileAtPos(x int, y int) *ChessTile {
+
+	//init things
+	piece := NewDefaultPieceForPosition(x, y)
+	button := widget.NewButton("", func() {
+		fmt.Println("button clicked", x, y)
+	})
+	//uiEL := container.NewVBox(piece.imageEL, button)
+	var uiEL *fyne.Container
+	if piece.imageEL == nil {
+		uiEL = container.NewVBox(button)
+	} else {
+		uiEL = container.NewHBox(piece.imageEL, button)
+	}
+
+	//create obj and return
+	return &ChessTile{
+		piece:  piece,
+		button: button,
+		uiEL:   uiEL,
+	}
+}
+
+type ChessBoard struct {
+	grid  *fyne.Container
+	tiles [8][8]*ChessTile
+}
+
+func NewChessBoard() *ChessBoard {
+	board := ChessBoard{}
+
+	//1d array for grid layout
+	tiles := make([]*fyne.CanvasObject, 64)
+
+	iter := 0
+	for x := 0; x < 8; x++ {
+		for y := 0; y < 8; y++ {
+
+			//init tile
+			tile := initChessTileAtPos(x, y)
+
+			//2d array for managing easier
+			board.tiles[x][y] = tile
+
+			//1d array for grid layout
+			tiles[iter] = tile.uiEL
+			iter++
+		}
+	}
+
+	grid := container.New(layout.NewGridLayout(8), tiles...)
+
 }
